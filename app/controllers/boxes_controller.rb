4397961420b6f8752429261706e845b1ffe8579d
Mylene Moreau <hello@mylenemoreau.fr>
2021-03-23 17:54:13 +0100

    controller box unfinished yet

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -3,6 +3,10 @@ class BoxesController < ApplicationController
     @box = Box.all
   end
 
+  def show
+    @box = Box.find(params[:id])
+  end
+
   def new
     @box = Box.new
   end
@@ -10,7 +14,7 @@ class BoxesController < ApplicationController
   def create
     @box = Box.new(box_params)
     if @box.save
-      redirect to boxes_path
+      redirect_to boxes_path
     else
       render :new
     end
@@ -21,17 +25,20 @@ class BoxesController < ApplicationController
   end
 
   def update
-  end
-
-  def show
+    @box = Box.find(params[:id])
+    @box.update(box_params)
+    redirect_to boxes_path
   end
 
   def destroy
+    @box = Box.find(params[:id])
+    @box.destroy
+    redirect_to boxes_path
   end
 
     private
 
   def box_params
-    params.require(:box).permit(:sound, :style)
+    params.require(:box).permit(:name, :sound, :style)
   end
 end

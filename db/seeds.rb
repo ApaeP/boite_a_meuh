ad48e785ea2405d736818bcb4f37e3f4024aaf64
Paul Portier <paulportier@gmail.com>
2021-03-25 17:37:03 +0100

    step 0

--- db/seeds.rb
+++ db/seeds.rb
@@ -92,7 +92,7 @@ puts "\n Building #{sounds_count.to_s} sounds"
   sounds_progress_bar = TTY::ProgressBar.new("-[:bar] :percent | ETA::eta | :rate/s", width: 80, total: sounds_count)
   animals.each do |animal|
     sound = Sound.new(name: animal)
-    sound.sound_file.attach(io: File.open(File.join(Rails.root,"app/assets/images/sound-#{animal}.mp3")), filename: "sound-#{animal}")
+    sound.sound_file.attach(io: File.open(File.join(Rails.root,"app/assets/sounds/sound-#{animal}.mp3")), filename: "sound-#{animal}")
     sound.save!
     sounds_progress_bar.advance(1)
   end
